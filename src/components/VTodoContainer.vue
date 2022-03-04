afd2bd31cca91618bb880ec7e01647ad38508094
Readpato <87768087+Readpato@users.noreply.github.com>
2022-03-04 17:27:17 +0100

    feat: Create container and header
    The container component and the header component have been created and mounted on the app

---
+++ src/components/VTodoContainer.vue
@@ -0,0 +1,13 @@
+<script>
+import VHeader from "./VHeader.vue";
+export default {
+  name: "VTodoContainer",
+  components: { VHeader },
+};
+</script>
+
+<template>
+  <div class="w-4/5 md:w-1/2 h-auto mx-auto bg-red-500 mt-10 rounded shadow-xl">
+    <VHeader title="Todo List App" />
+  </div>
+</template>
