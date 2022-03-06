f05f5e569f4fe2ce68d34d911066da234352adf4
Readpato <87768087+Readpato@users.noreply.github.com>
2022-03-06 19:21:02 +0100

    Feat: Add Pencil Icon
    The pencil icon that will serve to submit the task to the task list was added to the component

--- src/components/VForm.vue
+++ src/components/VForm.vue
@@ -1,19 +1,23 @@
 <script>
+import { PencilIcon } from "@heroicons/vue/outline";
+
 export default {
   name: "VForm",
+  components: { PencilIcon },
 };
 </script>
 
 <template>
   <form class="p-2 flex flex-col space-y-1">
     <label for="new-task" class="font-medium">New task:</label>
-    <div class="wrapper">
+    <div class="wrapper flex">
       <input
         id="new-task"
         type="text"
         class="p-1 w-4/5 border-rose-300 border rounded focus:outline-red-400 placeholder:italic placeholder:text-slate-400"
         placeholder="Write here"
       />
+      <PencilIcon class="w-6 h-auto ml-2" />
     </div>
   </form>
 </template>
