fbe34cf69d068f6dcf2060c37a4711b05918d317
Readpato <87768087+Readpato@users.noreply.github.com>
2022-03-16 19:26:57 +0100

    test: Add e2e for header component
    The Header app has been tested to assure that it works correctly

--- src/components/VHeader.spec.js
+++ src/components/VHeader.spec.js
@@ -4,6 +4,7 @@
 import { mount } from "@cypress/vue";
 import VHeader from "./VHeader.vue";
 
-it("Mounts", () => {
-  mount(VHeader);
+it("Mounts and displays a the correct text in the header", () => {
+  mount(VHeader, { props: { title: "This is a header" } });
+  cy.get("nav h2").should("contain", "This is a header");
 });
